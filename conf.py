7177ee7142b6abaebd0cac47c1d96dbe453889a4
Puneeth Chaganti <punchagan@muse-amuse.in>
2013-11-11 18:17:54 +0530

    Mon Nov 11 18:17:54 IST 2013

--- conf.py
+++ conf.py
@@ -563,7 +563,7 @@ BODY_END = """<script type="text/javascript">
 # Also, if you want to use a different timezone in some of your posts,
 # you can use W3C-DTF Format (ex. 2012-03-30T23:00:00+02:00)
 #
-# TIMEZONE = 'Europe/Zurich'
+TIMEZONE = 'Asia/Kolkata'
 
 # If webassets is installed, bundle JS and CSS to make site loading faster
 # USE_BUNDLES = True
