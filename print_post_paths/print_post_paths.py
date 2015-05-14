7f5dabc216f04ff09c6b10d319ed61b9d443a00d
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-05-14 10:55:14 +0530

    Thu May 14 10:55:14 IST 2015

---
+++ print_post_paths/print_post_paths.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2012-2013 Puneeth Chaganti, Roberto Alsina and others.
+
+# Permission is hereby granted, free of charge, to any
+# person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the
+# Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the
+# Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice
+# shall be included in all copies or substantial portions of
+# the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+# OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+""" A plugin that prints paths on new_post/existing_post events. """
+
+from __future__ import unicode_literals, print_function
+
+from os.path import abspath
+
+from blinker import signal
+
+from nikola.plugin_categories import SignalHandler
+
+class PrintPostPaths(SignalHandler):
+    """ A plugin that prints paths on new_post/existing_post events. """
+
+    def set_site(self, site):
+        self.site = site
+
+        for type_ in ('new_post', 'new_page', 'existing_post', 'existing_page'):
+            s = signal(type_)
+            s.connect(self.print_paths)
+
+    def print_paths(self, sender, path, meta_path=None):
+        print('PATH: {}'.format(abspath(path)))
+        if meta_path:
+            print('PATH: {}'.format(abspath(meta_path)))
