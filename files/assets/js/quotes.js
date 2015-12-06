083762f29c7f3d45b2462754b629526fe3812d2d
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-12-06 20:27:09 +0530

    Add permalinks when show all is clicked.

--- files/assets/js/quotes.js
+++ files/assets/js/quotes.js
@@ -4,32 +4,51 @@ var hide_all_quotes = function(quotes) {
     }
 }
 
-var show_all_quotes = function() {
-    var quotes = document.getElementsByClassName('quotes')[0].getElementsByClassName('org-ul')[0].getElementsByTagName('li');
+var show_all_quotes = function(event) {
+    if (event) {
+        event.preventDefault();
+    }
+    window.location.hash = '';
+    var quotes = document.querySelectorAll('.quotes .org-ul li');
     for (i=0; i<quotes.length; i++) {
-        quotes[i].hidden = false;
+        var q = quotes[i];
+        q.hidden = false;
+
+        if (!q.querySelector('.permalink')) {
+            var link = document.createElement("a");
+            link.setAttribute('href', window.location.pathname + '#' + i);
+            link.setAttribute('class', 'permalink');
+            link.setAttribute('onclick', 'show_random_quote(null,'+i+')');
+            link.textContent = "#";
+            q.appendChild(link);
+        }
     }
 }
 
-var show_random_quote = function(event) {
+var show_random_quote = function(event, i) {
 
     if (event) {
         event.preventDefault();
     }
 
-    var quotes = document.getElementsByClassName('quotes')[0].getElementsByClassName('org-ul')[0].getElementsByTagName('li');
+    var quotes = document.querySelectorAll('.quotes .org-ul li');
     hide_all_quotes(quotes);
 
     var index;
-    try {
-        index = Number(location.hash.match("#(.*)")[1]);
-    }
-    catch (e) {
-        index = Math.round(Math.random() * quotes.length) % quotes.length;
-    }
-    finally {
-        if (isNaN(index) || event) {
+
+    if (i!=undefined) {
+        index = i;
+    } else {
+        try {
+            index = Number(location.hash.match("#(.*)")[1]);
+        }
+        catch (e) {
             index = Math.round(Math.random() * quotes.length) % quotes.length;
+        }
+        finally {
+            if (isNaN(index) || event) {
+                index = Math.round(Math.random() * quotes.length) % quotes.length;
+            }
         }
     }
 
@@ -39,12 +58,11 @@ var show_random_quote = function(event) {
     // Change link for copy-paste, etc.
     window.location.hash = "#" + index;
 
-
 }
 
 var add_buttons = function () {
-    var quote_container = document.getElementsByClassName('quotes')[0];
-    var quotes = quote_container.getElementsByClassName('org-ul')[0].getElementsByTagName('li');
+    var quote_container = document.querySelector('.quotes');
+    var quotes = document.querySelectorAll('.quotes .org-ul li');
     var count = quotes.length;
 
     var nav = document.createElement("div");
@@ -69,7 +87,7 @@ var add_buttons = function () {
     var show_quotes = document.createElement("a");
     show_quotes.setAttribute('title', 'Show All Quotes');
     show_quotes.setAttribute('href', '');
-    show_quotes.setAttribute('onclick', 'show_all_quotes()');
+    show_quotes.setAttribute('onclick', 'show_all_quotes(event)');
     show_quotes.textContent = "Show All (" + count + ") Quotes";
     show.appendChild(show_quotes);
 
