08721474faf67d18945adbcdaf551fd86c2fb8e0
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-02-07 02:47:48 +0530

    Add script to update webmentions

---
+++ update-webmentions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -ex
+
+PUBLIC_DIR="public"
+GIT_URL=$(git remote get-url origin)
+
+# Ensure theme is using our local changes
+set +e
+grep theme.*\"er\" config.toml
+USING_ER_THEME=$?
+set -e
+if [ $USING_ER_THEME -eq 0 ]; then
+    grep -q develop themes/er/.git/HEAD
+fi
+
+# Build the site
+pushd $(dirname $0)
+rm -rf "${PUBLIC_DIR}"
+hugo --ignoreCache
+
+# Push to GitHub
+pushd "${PUBLIC_DIR}"
+git init
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force "${GIT_URL}" master:gh-pages
+popd
+
+popd
